73395e0504938435589a1738982428ff53a371f4
Didi <git@d10r.net>
2023-12-20 19:13:27 +0100

    fix typo

--- test/GDASemanticIssues.t.sol
+++ test/GDASemanticIssues.t.sol
@@ -17,7 +17,7 @@ function toU256(int96 i96) pure returns (uint256) {
 /*
  * This test contract makes the point that the currently implemented semantics of flow distribution is flawed.
  *
- * The pool state, defined by unit allocations and distribution flowrate, does not determine the effective flowrate (how much is being distirbuted to pool members).
+ * The pool state, defined by unit allocations and distribution flowrate, does not determine the effective flowrate (how much is being distributed to pool members).
  * Instead, the effective flowrate additionally depeonds on the past order of `distributeFlow` and `updateMemberUnits` actions.
  *
  * Definitions:
